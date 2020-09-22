3b292e7845c16aa227460194bb078045406e11da
vagrant <vagrant@localhost.localdomain>
2020-09-22 13:26:35 +0000

    check git push

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  root 'pblic/items#top'
+  devise_for :customers
+
+  get '/items/about' => 'public/items#about', as: 'customer_about'
+  devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/items' => 'public/items#index'
+  get '/items/:id' => 'public/items#show'
 end
